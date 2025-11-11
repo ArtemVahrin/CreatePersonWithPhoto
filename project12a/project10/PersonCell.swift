4cba757c9d7d01c33d55326e97e8efc827c3fb3f
ArtemVahrin <artemvahrin6@gmail.com>
2025-11-11 13:45:21 +0300

    Initial Commit

---
+++ project12a/project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Артём on 18.10.2025.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
